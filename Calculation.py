3963e1dda532d2a6a76f27a8a89f8133838a115b
NJUsunset <1772005963@qq.com>
2025-06-30 13:12:47 +0800

    a

--- Calculation.py
+++ Calculation.py
@@ -2,6 +2,7 @@ import numpy
 import pandas
 import math
 import os
+import shutil
 from pyproj import Geod
 
 CONFIG_PREFIX = './config/'
@@ -43,31 +44,29 @@ def calculation_setting():
     return depth_step, calculation_settings
 
 
-def build_grn_input(depth_number, calculation_settings, override):
+def build_grn_input(depth, calculation_settings):
     # build up psgrn input file in specified depth
 
-    dir = TEMP_PREFIX + 'grn/' + str(depth_number)
+    dir = TEMP_PREFIX + 'grn_input/' + str(depth)
+
+    os.makedirs(dir, exist_ok=True)
 
     # override option will override any possible exist former file in temp/grn/
     # or the function will skip writing if there is any file in target folder
-    if override:
-        os.system(f'[ -d "{dir}" ] || mkdir -p "{dir}"')
-    else:
-        if (len(os.listdir(dir)) != 0):
-            return 0
-        else:
-            os.system(f'[ -d "{dir}" ] || mkdir -p "{dir}"')
+    if (len(os.listdir(dir)) != 0):
+        print(f'INFO: grn input file for depth {depth} already exists, skipping...')
+        return 0
     
     # compose grn input file
-    with open(TEMP_PREFIX + 'grn_input/' + str(depth_number) + '.grn', 'w') as grn_input, \
+    with open(TEMP_PREFIX + 'grn_input/' + str(depth) + '.grn', 'a') as grn_input, \
         open(CONFIG_PREFIX + 'model.dat', 'r') as model:
-        grn_input.write(f'{depth_list[depth_number]} {calculation_settings[0][1]} \n')
+        grn_input.write(f'{depth} {calculation_settings[0][1]} \n')
         grn_input.write(f'{calculation_settings[1][0]} {calculation_settings[1][1]} {calculation_settings[1][2]} {calculation_settings[1][3]}\n')
         grn_input.write(f'{calculation_settings[2][0]} {calculation_settings[2][1]} {calculation_settings[2][2]}\n')
         grn_input.write(f'{calculation_settings[3][0]} {calculation_settings[3][1]}\n')
         grn_input.write(f'{calculation_settings[4][0]}\n')
         grn_input.write(f'{calculation_settings[5][0]}\n')
-        grn_input.write(f"'{TEMP_PREFIX}grn/{depth_number}/'\n")
+        grn_input.write(f"'{TEMP_PREFIX}grn/{depth}/'\n")
         grn_input.write("'uz' 'ur' 'ut'\n" \
                         "'szz' 'srr' 'stt' 'szr' 'srt' 'stz'\n" \
                         "'tr' 'tt' 'rot' 'gd' 'gr'\n")
@@ -79,6 +78,8 @@ def build_grn_input(depth_number, calculation_settings, override):
                 continue
             cleaned_line = ' '.join(stripped_line.split())
             grn_input.write(cleaned_line + '\n')
+        
+    return dir
 
 
 def observation_array_on_fault(receiving_fault_array, target_depth, observation_distance):
@@ -131,10 +132,10 @@ def config():
 
     return configs
 
-def build_cmp_input(depth_number, observation_distance, configs, override):
+def build_cmp_input(depth, observation_distance, configs, override):
     # build up pscmp input file
 
-    dir = TEMP_PREFIX + 'cmp/' + str(depth_number)
+    dir = TEMP_PREFIX + 'cmp/' + str(depth)
     
     # override option will override any possible exist former file in temp/cmp/
     # or the function will skip writing if there is any file in target folder
@@ -154,11 +155,11 @@ def build_cmp_input(depth_number, observation_distance, configs, override):
             if not stripped_line or stripped_line.startswith('#'):
                 continue
             split_line = stripped_line.split()
-            observation_points = observation_array_on_fault(split_line, depth_list(depth_number), observation_distance)
+            observation_points = observation_array_on_fault(split_line, depth, observation_distance)
             for point in observation_points:
                 observation_array.append(observation_points)
 
-    with open(TEMP_PREFIX + 'cmp_input/' + str(depth_number) + '.cmp', 'w') as cmp_input, \
+    with open(TEMP_PREFIX + 'cmp_input/' + str(depth) + '.cmp', 'w') as cmp_input, \
         open(CONFIG_PREFIX + 'source_fault.dat', 'r') as source_fault:
         cmp_input.write('0\n')
         cmp_input.write(f'{len(observation_array)}\n')
@@ -177,7 +178,7 @@ def build_cmp_input(depth_number, observation_distance, configs, override):
             cmp_input.write(f'{configs[1][3]} {configs[1][4]} {configs[1][5]} ')
             cmp_input.write(f'{configs[1][6]} {configs[1][7]} {configs[1][8]}\n')
         
-        cmp_input.write(f"'{TEMP_PREFIX}cmp/{depth_number}/'\n")
+        cmp_input.write(f"'{TEMP_PREFIX}cmp/{depth}/'\n")
         cmp_input.write('0 0 0\n')
         cmp_input.write("'ux.dat' 'uy.dat' 'uz.dat'\n")
         cmp_input.write('0 0 0 0 0 0\n')
@@ -191,7 +192,7 @@ def build_cmp_input(depth_number, observation_distance, configs, override):
             cmp_input.write(f'{configs[3 + snapshot_count][0]} {configs[3 + snapshot_count][1]}\n')
             snapshot_count += 1
         
-        cmp_input.write(f"'{TEMP_PREFIX}grn/{depth_number}/'\n")
+        cmp_input.write(f"'{TEMP_PREFIX}grn/{depth}/'\n")
         cmp_input.write("'uz' 'ur' 'ut'\n")
         cmp_input.write("'szz' 'srr' 'stt' 'szr' 'srt' 'stz'\n")
         cmp_input.write("'tr' 'tt' 'rot' 'gd' 'gr'\n")
@@ -207,23 +208,50 @@ def build_cmp_input(depth_number, observation_distance, configs, override):
 if __name__ == '__main__':
     print('INFO: setup all files in config folder before running this script.')
     print('INFO: calculate.py running...')
-    ifgrn = input('INFO: Do you want to calculate green function set? (y/n): ')
-    ifoverridegrn = input('INFO: Do you want to override existed green function set? (y/n): ')
-    ifcmp = input('INFO: Do you want to calculate deltacfs? (y/n): ')
-    ifoverridecmp = input('INFO: Do you want to override existed deltacfs? (y/n): ')
-    if ifgrn == 'y':
+    ifgrn = input('INFO: Do you want to calculate green function set? (y/no-override/n): ')
+
+    if ifgrn !='n' and ifgrn != 'no-override' and ifgrn != 'y':
+        print('ERROR: Bad input')
+        os._exit(0)
+
+    ifcmp = input('INFO: Do you want to calculate deltacfs? (y/no-override/n): ')
+
+    if ifcmp != 'y' and ifcmp != 'no-override' and ifcmp != 'n':
+        print('ERROR: Bad input')
+        os._exit(1)
+
+    if ifgrn != 'n':
         print('INFO: grn_input.py running...')
         depth_range = depth_minmax()
         depth_step, calculation_settings = calculation_setting()
-        overide = False
-        if ifoverridegrn == 'y':
-            overide = True
-        for depth_number in range(depth_range[0], depth_range[1] + 1):
-            build_grn_input(depth_number, calculation_settings, overide)
-    if ifcmp == 'y':
+        depth_array = []
+        depth = depth_range[0]
+        while depth <= depth_range[1]:
+            depth_array.append(depth)
+            depth += depth_step
+
+
+        if ifgrn == 'y':
+            print('INFO: Overriding existing green function set...')
+            shutil.rmtree(TEMP_PREFIX + 'grn/', ignore_errors=True)
+            shutil.rmtree(TEMP_PREFIX + 'grn_input/', ignore_errors=True)
+        
+        for depth in depth_array:
+            dir = build_grn_input(depth, calculation_settings)
+            state = 0
+            state = os.system('sh ./src/psgrn.sh ' + dir + '.grn')
+            if state != 0:
+                print(f'ERROR: psgrn.sh failed for depth {depth}.')
+                os._exit(1)
+            print(f'INFO: psgrn.sh finished for depth {depth}.')
+            
+
+    if ifcmp != 'n':
         print('INFO: cmp_input.py running...')
-        overide = False
-        if ifoverridecmp == 'y':
-            overide = True
-        build_cmp_input(overide)
+        if ifcmp == 'y':
+            print('INFO: Overriding existing deltacfs...')
+            shutil.rmtree(TEMP_PREFIX + 'cmp/', ignore_errors=True)
+            shutil.rmtree(TEMP_PREFIX + 'cmp_input/', ignore_errors=True)
+        build_cmp_input()
+    
     print('INFO: calculate.py finished.')
